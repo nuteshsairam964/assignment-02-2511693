8ee75b35372cb5da61918b7523c214d14d05f728
nuteshsairam964 <133896264+nuteshsairam964@users.noreply.github.com>
2026-03-13 13:27:30 +0530

    schema_design.sql

--- part1-rdbms/schema_design.sql
+++ part1-rdbms/schema_design.sql
@@ -1,1 +1,36 @@
+CREATE TABLE customers (
+customer_id INT PRIMARY KEY,
+customer_name VARCHAR(100),
+city VARCHAR(50)
+);
+
+CREATE TABLE products (
+product_id INT PRIMARY KEY,
+product_name VARCHAR(100),
+price DECIMAL(10,2)
+);
+
+CREATE TABLE sales_representatives (
+rep_id INT PRIMARY KEY,
+rep_name VARCHAR(100)
+);
+
+CREATE TABLE orders (
+order_id INT PRIMARY KEY,
+customer_id INT,
+rep_id INT,
+order_date DATE,
+FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+FOREIGN KEY (rep_id) REFERENCES sales_representatives(rep_id)
+);
+
+CREATE TABLE order_items (
+order_item_id INT PRIMARY KEY,
+order_id INT,
+product_id INT,
+quantity INT,
+price DECIMAL(10,2),
+FOREIGN KEY (order_id) REFERENCES orders(order_id),
+FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
 
